a77604dd00e97b16d1860e0ddb4479087447821e
Pixselve <pixselve@gmail.com>
2020-05-21 15:48:19 +0200

    Add a form with validation

--- components/CreateSkillDialog.vue
+++ components/CreateSkillDialog.vue
@@ -4,39 +4,57 @@
       <v-btn v-on="on" color="primary" block>Ajouter une compétence</v-btn>
     </template>
     <v-card>
-      <v-card-title>Ajouter une compétence</v-card-title>
-      <v-card-text>
-        <v-text-field v-model="skill" placeholder="Compétence"></v-text-field>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn @click="close" text color="primary">Annuler</v-btn>
-        <v-btn @click="submit" color="primary">Ajouter</v-btn>
-      </v-card-actions>
+      <v-form ref="form" @submit.prevent="submit">
+        <v-card-title>Ajouter une compétence</v-card-title>
+        <v-card-text>
+          <v-text-field
+            :rules="rules"
+            autofocus
+            outlined
+            v-model="skill"
+            label="Compétence"
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="close" text color="primary">Annuler</v-btn>
+          <v-btn type="submit" color="primary">Ajouter</v-btn>
+        </v-card-actions>
+      </v-form>
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator';
+import { Component, Ref, Vue } from "vue-property-decorator";
 
-  @Component({})
-  export default class CreateSkillDialog extends Vue {
-    dialog = false;
-    skill = "";
+@Component({})
+export default class CreateSkillDialog extends Vue {
+  @Ref("form") readonly form!: HTMLFormElement;
+  dialog = false;
+  skill = "";
 
-    reset() {
-      this.skill = "";
-    }
 
-    close() {
-      this.dialog = false;
-      this.reset();
-    }
+  rules = [
+    (v: string) => v.length > 0 || "Veuillez entrer un nom"
+  ];
 
-    submit() {
+  reset() {
+    this.skill = "";
+    this.form.resetValidation();
+  }
+
+  close() {
+    this.dialog = false;
+    this.reset();
+  }
+
+  submit() {
+    if (this.form.validate()) {
       this.$emit("submit", this.skill);
       this.close();
     }
+
   }
+}
 </script>
