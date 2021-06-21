e5e0a5d215c55a1350dd5e0f449ea768ea6807a0
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-06-21 14:01:27 +0200

    :sparkles: edit student skills

--- components/StudentSkillTableRow.vue
+++ components/StudentSkillTableRow.vue
@@ -1,7 +1,7 @@
 <template>
   <tr>
     <th class="text-no-wrap">{{student.lastName}} {{student.firstName}}</th>
-    <student-skill-table-cell :skill="skill" :student="student" v-for="skill in skills" :key="skill.id"></student-skill-table-cell>
+    <student-skill-table-cell :contract-i-d="contractID" v-on:update="() => $emit('update')" :skill="skill" :student="student" v-for="skill in skills" :key="skill.id"></student-skill-table-cell>
 <!--    <td v-for="skill in skills" :key="skill.id" class="red lighten-3">-->
 <!--      <v-select class="mt-6" flat dense outlined :items="selectValues"></v-select>-->
 <!--    </td>-->
@@ -21,7 +21,7 @@
   export default class StudentSkillTableRow extends Vue {
     @Prop({ type: Object }) readonly student!: Student;
     @Prop({ type: Array }) readonly skills!: Skill
-
+    @Prop() readonly contractID!: number;
 
 
 
--- pages/teacher/contracts/_id.vue
+++ pages/teacher/contracts/_id.vue
@@ -1,42 +1,45 @@
 <template>
   <div>
-        <v-app-bar
-        >
-          <v-app-bar-nav-icon to="/teacher/contracts">
-            <v-icon>mdi-arrow-left</v-icon>
-          </v-app-bar-nav-icon>
+    <v-app-bar
+    >
+      <v-app-bar-nav-icon to="/teacher/contracts">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-app-bar-nav-icon>
 
-          <v-toolbar-title>{{contract.name}} ({{ getFormattedDate(contract.start) }} -
-            {{ getFormattedDate(contract.end) }})
-          </v-toolbar-title>
+      <v-toolbar-title>{{ contract.name }} ({{ getFormattedDate(contract.start) }} -
+        {{ getFormattedDate(contract.end) }})
+      </v-toolbar-title>
 
-        </v-app-bar>
-        <v-container fluid>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-              <tr>
-                <td class="text-left">Élève</td>
-                <th v-for="skill in contract.skills" :key="skill.id" class="text-left">{{skill.name}}</th>
-              </tr>
-              </thead>
-              <tbody>
-              <student-skill-table-row :skills="contract.skills" v-for="student in studentsConcernedByTheContract"
-                                       :student="student"
-                                       :key="student.ownerUsername"></student-skill-table-row>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-container>
+    </v-app-bar>
+    <v-container fluid>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+          <tr>
+            <td class="text-left">Élève</td>
+            <th v-for="skill in contract.skills" :key="skill.id" class="text-left">{{ skill.name }}</th>
+          </tr>
+          </thead>
+          <tbody>
+          <student-skill-table-row :skills="contract.skills"
+                                   v-for="(student, i) in studentsConcernedByTheContract"
+                                   :student="student"
+                                   :contract-i-d="contract.id"
+                                   :key="student.ownerUsername"></student-skill-table-row>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import StudentSkillTableRow from "~/components/StudentSkillTableRow.vue";
-import { FetchContractQuery, FetchStudentForContractQuery, Group } from "~/types/types";
+import { FetchContractQuery, FetchStudentForContractQuery, FetchStudentQuery, Group } from "~/types/types";
 import FetchContractGQL from "~/apollo/queries/FetchContract.graphql";
 import FetchStudentForContractGQL from "~/apollo/queries/FetchStudentForContract.graphql";
+import FetchStudentGQL from "~/apollo/queries/FetchStudent.graphql";
 
 @Component<TeacherContractPage>({
   layout: "teacher",
@@ -49,17 +52,21 @@ import FetchStudentForContractGQL from "~/apollo/queries/FetchStudentForContract
   async asyncData({ app, params }) {
     const { id } = params;
     const apolloClient = app.apolloProvider.defaultClient;
-    const { data } = await apolloClient.query({ query: FetchContractGQL, variables: {id} });
-    const {data: studentData} = await apolloClient.query({query: FetchStudentForContractGQL, variables: {contractID: id}})
+    const { data } = await apolloClient.query({ query: FetchContractGQL, variables: { id } });
+    const { data: studentData } = await apolloClient.query({
+      query: FetchStudentForContractGQL,
+      variables: { contractID: id }
+    });
     return {
       contract: data.contract,
       studentsConcernedByTheContract: studentData.students
-    }
+    };
   },
 })
 export default class TeacherContractPage extends Vue {
-  contract!: FetchContractQuery["contract"]
-  studentsConcernedByTheContract: FetchStudentForContractQuery["students"] = []
+  contract!: FetchContractQuery["contract"];
+  studentsConcernedByTheContract: FetchStudentForContractQuery["students"] = [];
+
   getFormattedDate(date: string) {
     return new Date(date).toLocaleDateString("fr-FR", {
       day: "numeric",
