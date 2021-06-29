ada62f05263e56a954bc81aeb888418da600d6b3
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-06-29 13:02:53 +0200

    :sparkles: create a student

--- components/CreateStudentDialog.vue
+++ components/CreateStudentDialog.vue
@@ -11,52 +11,103 @@
         <v-card-title>
           Ajouter un élève
         </v-card-title>
-        <v-card-text>
-          <v-text-field v-model="studentData.firstName" outlined required placeholder="Prénom"></v-text-field>
-          <v-text-field v-model="studentData.lastName" outlined required placeholder="Nom de famille"></v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn @click="close" color="primary" text>Annuler</v-btn>
-          <v-btn type="submit" color="primary">Ajouter</v-btn>
-        </v-card-actions>
+        <div v-if="!complete">
+          <v-card-text>
+            <v-text-field v-model="studentData.firstName" outlined required label="Prénom"></v-text-field>
+            <v-text-field v-model="studentData.lastName" outlined required label="Nom de famille"></v-text-field>
+            <v-text-field v-model="studentData.password" outlined required label="Mot de passe"></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="close" color="primary" text>Annuler</v-btn>
+            <v-btn type="submit" color="primary">Ajouter</v-btn>
+          </v-card-actions>
+        </div>
+        <div v-else-if="addedStudent !== null">
+          <v-card-text>
+            <v-alert type="success">
+              Élève ajouté !
+              <v-divider></v-divider>
+              Identifiant : {{ addedStudent.ownerUsername }}
+            </v-alert>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="close" color="primary" text>Fermer</v-btn>
+          </v-card-actions>
+
+        </div>
       </v-form>
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
-  import { Component, Ref, Vue } from 'vue-property-decorator';
+import { Component, Ref, Vue } from 'vue-property-decorator';
+import CreateOneStudentGQL from "~/apollo/mutations/student/CreateOneStudent.graphql";
+import { CreateOneStudentMutation, FetchStudentsQuery } from "~/types/types";
+import FetchStudentsQueryGQL from "~/apollo/queries/FetchStudents.graphql";
 
-  @Component({})
-  export default class CreateStudentDialog extends Vue {
+@Component({})
+export default class CreateStudentDialog extends Vue {
 
-    @Ref("form") readonly formRef!: HTMLFormElement;
-    dialog = false;
-    loading = false;
+  @Ref("form") readonly formRef!: HTMLFormElement;
+  dialog = false;
+  loading = false;
+  complete = false;
+  addedStudent: CreateOneStudentMutation["createOneStudent"] | null = null;
 
 
-    studentData = {
+  studentData = {
+    firstName: "",
+    lastName: "",
+    password: ""
+  };
+
+  close() {
+    this.dialog = false;
+    this.formRef.reset();
+    this.studentData = {
       firstName: "",
-      lastName: ""
+      lastName: "",
+      password: ""
     };
-
-    close() {
-      this.dialog = false;
-      this.formRef.reset();
-    }
-
-    async submit() {
-      // TODO
-      // this.loading = true;
-      // try {
-      //   await studentsStore.addStudent(this.studentData);
-      //   this.close();
-      // } catch (e) {
-      //   console.error(e);
-      // } finally {
-      //   this.loading = false;
-      // }
-    }
+    this.addedStudent = null;
+    this.complete = false;
   }
+
+  async submit() {
+    try {
+      this.loading = true;
+      const { data: addedStudent } = await this.$apollo.mutate({
+        mutation: CreateOneStudentGQL,
+        variables: this.studentData,
+        update: (store, { data: { createOneStudent } }) => {
+          const data: FetchStudentsQuery | null = store.readQuery({
+            query: FetchStudentsQueryGQL,
+          });
+          if (data !== null) {
+            data.students.push(createOneStudent);
+            store.writeQuery({ query: FetchStudentsQueryGQL, data });
+          }
+        }
+      });
+      this.addedStudent = addedStudent.createOneStudent;
+      this.complete = true;
+      this.loading = false;
+    } catch (e) {
+
+    }
+    // TODO
+    // this.loading = true;
+    // try {
+    //   await studentsStore.addStudent(this.studentData);
+    //   this.close();
+    // } catch (e) {
+    //   console.error(e);
+    // } finally {
+    //   this.loading = false;
+    // }
+  }
+}
 </script>
